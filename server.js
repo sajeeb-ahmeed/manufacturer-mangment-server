acecb441354a0d116d32680e099ee97d9fe472d8
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-05-26 18:01:35 +0600

    server tools api

--- server.js
+++ server.js
@@ -11,6 +11,23 @@ app.use(cors());
 app.use(express.json());
 
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "Unauthorized Access" });
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: "Forbidden access" });
+        }
+        console.log("decoded", decoded);
+        req.decoded = decoded;
+        next();
+    });
+}
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wbiyf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -21,14 +38,30 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        const inventoryCollection = client.db('sareen').collection('inventory');
-        console.log('db connected');
-        //SERVICES API
-        app.get('/services', async (req, res) => {
-            const cursor = inventoryCollection.find()
-            const services = await cursor.toArray();
-            res.send(services)
+        console.log("Connected to MongoDB");
+        const db = client.db("toolsDB");
+        const toolsCollection = db.collection("toolsCollection");
+
+        // API to Run Server
+        app.get("/", async (req, res) => {
+            res.send("Manufacturer Server Running");
         });
+
+        //API to get all tools
+        app.get("/tools", async (req, res) => {
+            const tools = await toolsCollection.find({}).toArray();
+            res.send(tools);
+        });
+
+        //API to get single tools
+        app.get("/tools/:id", async (req, res) => {
+            const id = req.params.id;
+            const tool = await toolsCollection.findOne({ _id: ObjectId(id) });
+            res.send(tool);
+        });
+
+
+
 
     }
     finally {
