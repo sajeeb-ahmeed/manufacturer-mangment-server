ff189b49279c59bea2f5cd8e347d9c03fc1c0fcf
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-05-26 18:10:48 +0600

    payment done

--- server.js
+++ server.js
@@ -59,21 +59,21 @@ const run = async () => {
         };
 
         // API for payment
-        // app.post("/create-payment-intent", async (req, res) => {
-        //   const { totalPrice } = req.body;
-        //   const amount = parseInt(totalPrice) * 100;
-        //   // Create a PaymentIntent with the order amount and currency
-        //   const paymentIntent = await stripe.paymentIntents.create({
-        //     amount: amount,
-        //     currency: "usd",
+        app.post("/create-payment-intent", async (req, res) => {
+            const { totalPrice } = req.body;
+            const amount = parseInt(totalPrice) * 100;
+            // Create a PaymentIntent with the order amount and currency
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
 
-        //     payment_method_types: ["card"]
-        //   });
+                payment_method_types: ["card"]
+            });
 
-        //   res.send({
-        //     clientSecret: paymentIntent.client_secret,
-        //   });
-        // });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
 
         //API to post a user
         app.put("/user/:email", async (req, res) => {
